ef64075c1da628c100c4f536731faf4715a23e99
Jane Jeon <ho@ho.ca>
2017-02-17 11:13:34 -0500

    Added basic input/response loop. It's technically a terminal now!

---
+++ src/main/java/Input.java
@@ -0,0 +1,11 @@
+public class Input {
+	public static void readTest(String s) {
+		System.out.println(s);
+	}
+	
+	public static String response(String s) {
+		String result = "This is a response to your input!\n";
+		System.out.println(s);
+		return result;
+	}
+}
--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -3,6 +3,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Screen;
@@ -11,23 +12,38 @@ import javafx.stage.StageStyle;
 
 public class Launch extends Application {
 	private double width = 960, height = 600;
+	private String imgPath = "background.jpg";
+	private TextArea textArea;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	@Override
 	public void start(Stage primaryStage) {
 		VBox root = new VBox();
 
-		BackgroundImage bg = new BackgroundImage(new Image("background.jpg",960,600,false,true), 
+		BackgroundImage bg = new BackgroundImage(new Image(imgPath, width, height, false, true), 
 				BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, 
 				BackgroundSize.DEFAULT);
-
-		TextArea textArea = new TextArea();
+		
+		textArea = new TextArea();
 		textArea.setWrapText(true);
 		textArea.setPrefSize(height, width);
-		textArea.setFont(Font.font("Lucida Grande", 22));
+		textArea.setFont(Font.font("Lucida Grande", 22)); // try to keep width at most 75 characters
+		
+		// default dialog
+		textArea.appendText("Hello, human!\n");
+		
+		textArea.setOnKeyPressed(event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				String response = Input.response(textArea.getText());
+				textArea.setText(response);
+				// reset caret at the end
+				textArea.positionCaret(response.length());
+			}
+			// consume event to prevent another line from being added
+			event.consume();
+		});
 		
 		root.setBackground(new Background(bg));
 		root.getChildren().add(textArea);
